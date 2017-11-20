3dcb24f3e06fd19820bdfa47879665babc7dd33c
longlg88 <longlg88@naver.com>
2017-11-21 01:08:23 +0900

    Make slack server & communication with slack app

--- health_check/check.py
+++ health_check/check.py
@@ -1,18 +1,49 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 from slackclient import SlackClient
+
+def handle_command(channel):
+	response='hello world!'
+	slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+
+def parse_slack_output(slack_rtm_output):
+	output_list=slack_rtm_output
+	if output_list and len(output_list) > 0:
+		for output in output_list:
+			if output and 'text' in output and AT_BOT in output['text']:
+				return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
+	#for output in output_list:
+	#	if 'channel' in output:
+	#		return None, output['channel']
+	return None, None
 
 BOT_NAME='sysagent_healthcheck'
 
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
+BOT_ID=os.environ.get("BOT_ID")
+AT_BOT = "<@"+BOT_ID+">"
+
 if __name__=="__main__":
-	api_call = slack_client.api_call("users.list")
-	if api_call.get('ok'):
-		users=api_call.get('members')
-		for user in users:
-			if 'name' in user and user.get('name') == BOT_NAME:
-				print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+################################# BOT_ID CALL code
+#	api_call = slack_client.api_call("users.list")
+#	if api_call.get('ok'):
+#		users=api_call.get('members')
+#		for user in users:
+#			if 'name' in user and user.get('name') == BOT_NAME:
+#				print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+#################################
+	if slack_client.rtm_connect():
+		print(slack_client.rtm_read())
+		READ_WEBSOCKET_DELAY=1
+		print("start bot!! BOT NAME is " + BOT_NAME)
+		while True:
+			command,  channel = parse_slack_output(slack_client.rtm_read())
+			print command, channel
+			if channel:
+				handle_command(channel)
+			time.sleep(READ_WEBSOCKET_DELAY)
 	else:
 		print("could not find bot user with the name " + BOT_NAME)
